c0e6a67cddf2fb14af51f3fe8932de4a374b5262
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-28 13:10:13 +0500

    add desklist

--- app/Http/Requests/DeskListStoreRequest.php
+++ app/Http/Requests/DeskListStoreRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Desk;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRequest extends FormRequest
+class DeskListStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +22,8 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255|unique:desks,name,'.$this->desk->id
+            'name' => 'required|max:255|unique:desk_lists,name',
+            'desk_id' => 'required|max:255|exists:desks,id'
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\DeskController;
+use App\Http\Controllers\Api\DeskListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResources([
-    'desks' => DeskController::class
+    'desks' => DeskController::class,
+    'desk-lists' => DeskListController::class
 ]);
