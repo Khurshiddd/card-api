387d101057743b5b21a6d1178e96752916f81c6b
Khurshiddd <marifovxurshid80@gmail.com>
2023-10-03 13:06:49 +0500

    update card

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Card\StoreRequest;
+use App\Http\Resources\CardResource;
 use App\Models\Card;
 use Illuminate\Http\Request;
 
@@ -30,17 +31,18 @@ class CardController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Card $card)
     {
-        //
+        return $this->response(new CardResource($card));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreRequest $request, Card $card)
     {
-        //
+        $card->update($request->validated());
+        return $this->success('successfully updated');
     }
 
     /**
